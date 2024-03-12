1ba870be38e3a65bbbfc44469950a98b2620f61c
jamesnulliu <jamesnulliu@outlook.com>
2024-03-12 23:31:29 +0800

    Update ./scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 PROJECT_ROOT_DIR=$(pwd)
 
+rm -rf $PROJECT_ROOT_DIR/build
+
 mkdir -p $PROJECT_ROOT_DIR/build
 
 cd $PROJECT_ROOT_DIR/build
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,5 @@
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
-    int* arr;
-    if (1) {
-        std::cout << "This is a test" << std::endl;
-    }
     return 0;
 }
