757a164d367aaea8914e1e7511013dfbb6608f31
jamesnulliu <jamesnulliu@gmail.com>
2024-03-27 15:33:34 +0800

    Fix Bug in PreDefined.hpp

--- include/plazy/Common/PreDefined.hpp
+++ include/plazy/Common/PreDefined.hpp
@@ -7,13 +7,13 @@ namespace plazy
 {
 constexpr auto PLAZY_EOF = std::char_traits<char>::eof();
 
-constexpr std::array<std::string, 13> KEWORDS_STR = {
+constexpr std::array<std::string_view, 13> KEWORDS_STR = {
     "const", "var", "procedure", "begin", "end",   "if",  "then",
     "while", "do",  "call",      "odd",   "write", "read"};
 
 constexpr std::array<char, 9> OPERATORS_CH = {'+', '-', '*', '/', '=', '#', '>', '<', ':'};
 
-constexpr std::array<std::string, 11> OPERATORS_STR = {
+constexpr std::array<std::string_view, 11> OPERATORS_STR = {
     "+", "-", "*", "/", "=", "#", "<", ">", "<=", ">=", ":="};
 
 constexpr std::array<char, 5> DELIMITERS_CH = {'(', ')', ',', ';', '.'};
